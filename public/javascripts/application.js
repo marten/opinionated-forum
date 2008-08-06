5e93b479e409ff5526410a7a4fbaaaf919440c2a
Marten Veldthuis <marten@veldthuis.com>
2008-08-06 14:33:59 +0200

    moved some javascript to application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function toggle_startnewtopic() {
+  Effect.toggle('newtopic', 'slide', { duration: 0.2 })
+}
+
+function switch_to_slowreply() {
+  Effect.SlideDown("slowreply", { duration: 0.2 });
+  Effect.Fade("quickreply",     { duration: 0.2 });
+  $('post_body').focus();
+}
+
+function switch_to_quickreply() {
+  Effect.Appear("quickreply", { duration: 0.2 });
+  Effect.SlideUp("slowreply", { duration: 0.2 });
+  $('post_body').focus();
+}
